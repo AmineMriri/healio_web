e2cab5f279b641fa431caafe1cb2313b748938d4
Cyclone911 <aminemriri0@gmail.com>
2024-08-23 15:32:02 +0100

    doctor item

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -49,9 +49,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "29832c9559f4364ef0910372ec8e9a95
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "58fd8280ecefac8416a36bcc117373eb",
-"/": "58fd8280ecefac8416a36bcc117373eb",
-"main.dart.js": "ea3a06bb77364474d359c5e2744f0e36",
+"index.html": "9b2ebb20b0019808ac2f3489a4f90e9d",
+"/": "9b2ebb20b0019808ac2f3489a4f90e9d",
+"main.dart.js": "3abe52c607d1603220745a1478d5c2da",
 "manifest.json": "cea679379a0c96deabaf3575c2f5f6cc",
 "version.json": "349458cd45e2279f02cf49ea2dee6c25"};
 // The application shell files that are downloaded before a service worker can
