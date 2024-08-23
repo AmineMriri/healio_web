b3a9f7502a19aa31715ea9e9afcc816b6d181ea9
Cyclone911 <aminemriri0@gmail.com>
2024-08-23 16:21:58 +0100

    doc fix item 8

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -49,8 +49,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "29832c9559f4364ef0910372ec8e9a95
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "496bed6e0007eaef9775b856b4363710",
-"/": "496bed6e0007eaef9775b856b4363710",
+"index.html": "e9c529533b0853de968a3181b2e72562",
+"/": "e9c529533b0853de968a3181b2e72562",
 "main.dart.js": "cfd501a5b6dac4d9a0ed38a5a460427b",
 "manifest.json": "cea679379a0c96deabaf3575c2f5f6cc",
 "version.json": "349458cd45e2279f02cf49ea2dee6c25"};
