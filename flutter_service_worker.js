82943b79a8ad9aa359c5e8d90c2b9faa6ad236f0
Amine Mriri <aminemriri0@gmail.com>
2024-12-03 12:12:11 +0100

    Logo + attach BS

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "54e79a3567b0b52ed627dfb242e1785f
 "assets/assets/svg/signout.svg": "9c4b12b0f136566991658953f4fe71d9",
 "assets/FontManifest.json": "76472d16c92887edd6fddad0fab1167f",
 "assets/fonts/MaterialIcons-Regular.otf": "1565c99557d8b2d9bc6cb246b6f2b6e8",
-"assets/NOTICES": "bd94d3bda685cbd3b8597ff81a019bfa",
+"assets/NOTICES": "4f9a1fffa8b92cf0fceb5b8d60b0c4bf",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/fluttertoast/assets/toastify.js": "56e2c9cedd97f10e7e5f1cebd85d53e3",
@@ -55,9 +55,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "54e79a3567b0b52ed627dfb242e1785f
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9d518e5ede20b6860596e75f4c5c17bc",
-"/": "9d518e5ede20b6860596e75f4c5c17bc",
-"main.dart.js": "db86c58ddc170a5ed2c0013d966538e0",
+"index.html": "3997c2149b73a0ae0e30f774b9c28fe8",
+"/": "3997c2149b73a0ae0e30f774b9c28fe8",
+"main.dart.js": "63a19462488c538f8509b05ae2405680",
 "manifest.json": "cea679379a0c96deabaf3575c2f5f6cc",
 "version.json": "349458cd45e2279f02cf49ea2dee6c25"};
 // The application shell files that are downloaded before a service worker can
