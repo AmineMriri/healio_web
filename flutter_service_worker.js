2698c9b0cd88557148a861f058cf01072d8174f4
Amine Mriri <aminemriri0@gmail.com>
2024-12-04 22:54:56 +0100

    app-mobile

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -55,8 +55,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "54e79a3567b0b52ed627dfb242e1785f
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a0b6d1dad223cbf3dddb9d57403d62b2",
-"/": "a0b6d1dad223cbf3dddb9d57403d62b2",
+"index.html": "4ee2beea09de48f90a73b5f076f3ae1a",
+"/": "4ee2beea09de48f90a73b5f076f3ae1a",
 "main.dart.js": "ed17f5b2f46bb02bc3c22c5c1c7d72bc",
 "manifest.json": "cea679379a0c96deabaf3575c2f5f6cc",
 "version.json": "349458cd45e2279f02cf49ea2dee6c25"};
