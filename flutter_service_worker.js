f63ccd8bc47776900e77d12dee3f76a3448c15ee
Amine Mriri <aminemriri0@gmail.com>
2024-12-04 14:47:16 +0100

    change nbr docs per page 1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -55,8 +55,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "54e79a3567b0b52ed627dfb242e1785f
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "daea4b7e40be9327bae8d51f08035ffc",
-"/": "daea4b7e40be9327bae8d51f08035ffc",
+"index.html": "3e86ad5a59ab21ba822644e380466095",
+"/": "3e86ad5a59ab21ba822644e380466095",
 "main.dart.js": "25e7ef4ec4d3feb6edb3252c730d189c",
 "manifest.json": "cea679379a0c96deabaf3575c2f5f6cc",
 "version.json": "349458cd45e2279f02cf49ea2dee6c25"};
