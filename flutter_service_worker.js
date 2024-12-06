57bda16752f5d756c759ae2cd38cf5eade458f9a
meriambi <meriambenida@gmail.com>
2024-12-06 16:45:41 +0100

    fixed routing issue auth uis

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "54e79a3567b0b52ed627dfb242e1785f
 "assets/assets/svg/profile-svg.svg": "890209205cdee1e35f057639111f1e44",
 "assets/assets/svg/signout.svg": "9c4b12b0f136566991658953f4fe71d9",
 "assets/FontManifest.json": "76472d16c92887edd6fddad0fab1167f",
-"assets/fonts/MaterialIcons-Regular.otf": "f9100d14adcee7674f2d0a89a4cf3d95",
+"assets/fonts/MaterialIcons-Regular.otf": "608d2ab4aee03ea7fb9e377d04061fcc",
 "assets/NOTICES": "4f9a1fffa8b92cf0fceb5b8d60b0c4bf",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
@@ -55,9 +55,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "54e79a3567b0b52ed627dfb242e1785f
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "93b12ad9a18809d536a2f3fbaae452d2",
-"/": "93b12ad9a18809d536a2f3fbaae452d2",
-"main.dart.js": "8c427fde9fa79b43bd0bbad181f3f233",
+"index.html": "d8d4d67dc806437e03c64c18ffc840bc",
+"/": "d8d4d67dc806437e03c64c18ffc840bc",
+"main.dart.js": "cd280133f502343bb7cb8f9eee606da0",
 "manifest.json": "cea679379a0c96deabaf3575c2f5f6cc",
 "version.json": "349458cd45e2279f02cf49ea2dee6c25"};
 // The application shell files that are downloaded before a service worker can
