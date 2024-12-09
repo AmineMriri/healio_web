82e711e0ffd0412cede92ef8923271eb267e5a2f
meriambi <meriambenida@gmail.com>
2024-12-09 16:38:14 +0100

    optimizing try fail

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -55,8 +55,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "54e79a3567b0b52ed627dfb242e1785f
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c9c31c8d6aa5455d503fc809fddc8553",
-"/": "c9c31c8d6aa5455d503fc809fddc8553",
+"index.html": "19bcb94f09773ff45ce0cc01f193df5c",
+"/": "19bcb94f09773ff45ce0cc01f193df5c",
 "main.dart.js": "de82c5032dccb72c26228b2bae5da2e1",
 "manifest.json": "cea679379a0c96deabaf3575c2f5f6cc",
 "version.json": "349458cd45e2279f02cf49ea2dee6c25"};
