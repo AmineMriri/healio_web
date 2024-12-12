15cba9d779e915ffccdd7c76a07d1de08bfb51c7
meriambi <meriambenida@gmail.com>
2024-12-12 12:51:44 +0100

    fix set new pwd gst

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -45,8 +45,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "2bb5f122855af32ce5c1b7fa7fa8c93f
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e3f3e30746ca597310323e01cce65fb9",
-"/": "e3f3e30746ca597310323e01cce65fb9",
+"index.html": "873e4d057902a9547ae6d056aeb29698",
+"/": "873e4d057902a9547ae6d056aeb29698",
 "main.dart.js": "99f6cb0546029292f1196c252ad34c35",
 "manifest.json": "cea679379a0c96deabaf3575c2f5f6cc",
 "version.json": "349458cd45e2279f02cf49ea2dee6c25"};
